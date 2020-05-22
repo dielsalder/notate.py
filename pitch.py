5918d8551d7af275bc4b6deb69946625acaf940d
yhe <ehe@mit.edu>
2020-05-21 20:50:28 -0400

    changed to segment data type

--- pitch.py
+++ pitch.py
@@ -3,15 +3,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-class Segment:
+class Segment():
     """
-    start: int, start time
-    end: int, end time
-    frames: list of dict of frame properties"""
+    start: float, start time
+    end: float, end time
+    frames: dict of frame properties"""
+    start:float
+    end:float
+    frames:dict
     def __init__(self,start, end, frames):
         self.start = start
         self.end = end
         self.frames = frames
+
+    def __iter__(self):
+        return iter((self.start,self.end))
+
+    def __repr__(self):
+        return str((self.start,self.end))
+    
+    def __str__(self):
+        return repr(self)
     
 def draw_pitch(pitch):
     # taken from parselmouth docs
@@ -34,7 +46,7 @@ def draw_intensity(intensity):
 
 def sound_properties(sound, voiced_frames_only = True, time_step = None):
     """Generate an array containing a vector of pitch, loudness and voiced/unvoiced decision for each frame"""
-    properties_arr = []
+    frames = {}
     pitch = sound.to_pitch()
     intensity = sound.to_intensity()
     end_time = sound.get_end_time()
@@ -45,43 +57,45 @@ def sound_properties(sound, voiced_frames_only = True, time_step = None):
         pitch_value = pitch.get_value_at_time(time)
         intensity_value = intensity.get_value(time)
         is_voiced = not np.isnan(pitch_value)
-        frame_properties = {
-            "time":time,
+        frames[time] = {
             "intensity": intensity_value,
             "pitch":pitch_value,
             "is_voiced":is_voiced
         }
-        properties_arr.append(frame_properties)
-    voiced_only = tuple(frame for frame in properties_arr if frame["is_voiced"])
+    voiced_only = {time:props for time,props in frames.items() if props["is_voiced"]}
     # unvoiced_only = tuple(frame for frame in properties_arr if not frame["is_voiced"])
     # print(set(frame["intensity"] for frame in unvoiced_only))
     # intensity has value only in unvoiced frames??
     if voiced_frames_only:
         return voiced_only
     else:
-        return tuple(properties_arr)
+        return frames
 
 
-def note_segments(sound_properties):
+def note_segments(frames):
     """ takes a vector of sound properties and produces an array of timestamps (start, end) corresponding to note segments"""
     note_segments = []
     potential_min_or_max = None
     # either 'max' or 'min'
     note_start = 0
     search_mode = "max"
+    current_segment_frames = {}
     # rewrite this w no repetition if I have time
-    for frame in sound_properties:
+    for time,frame in frames.items():
         if np.isnan(frame["intensity"]):
             continue
         if not potential_min_or_max:
             potential_min_or_max = frame["intensity"]
+            current_segment_frames[time]=frame
 
         if (search_mode == "max" and frame["intensity"] > potential_min_or_max
         or search_mode=="min" and frame["intensity"] < potential_min_or_max):
-            note_segments.append((note_start, frame["time"]))
-            note_start = frame["time"]
+            # note_segments.append((note_start, time))
+            note_segments.append(Segment(note_start,time, current_segment_frames))
+            note_start = time
             potential_min_or_max = frame["intensity"]
             search_mode="min" if search_mode=="max" else "max"
+            current_segment_frames={}
     return note_segments
 
 
